7788500b01fa05f680b0379a35cb5d0f5c6b5410
saianudeepgit <saianudeepgajula@gmail.com>
2023-10-26 14:17:00 +0900

    functions

---
+++ function.py
@@ -0,0 +1,13 @@
+def i_like(gamename,playthrough):
+    print("hey i like the game "+gamename+" and i have played it "+playthrough+" times")
+
+i_like("God of war 2018","25")
+i_like("Spiderman-Miles Morales","10")
+i_like("Ghost of tsushima","15")
+
+
+def square(num):
+    return num*num
+print(square(5))
+print(square(2))
+print(square(3))
